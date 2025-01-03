60b19696e5586cdadc76062faa729071a2000c4c
StrigZ <mirgasan200@gmail.com>
2025-01-03 19:31:36 +0300

    Fetch data on form submit and render weather card component

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 import '../styles/reset.css'
 import '../styles/styles.css'
+import { getWeather } from './getWeather'
+import { renderWeatherCard } from './renderWeatherCard'
 
-console.log('HEY')
+const searchQueryInput = document.querySelector('input')
+const searchQueryForm = document.querySelector('form')
+
+searchQueryForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  getWeather(searchQueryInput.value).then((data) => renderWeatherCard(data))
+})
