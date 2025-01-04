2005846dd584ed1dbf875ee170ab6dce6631c9e2
StrigZ <mirgasan200@gmail.com>
2025-01-04 14:57:57 +0300

    Add gif support for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
         loader: 'html-loader'
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource'
       },
       {
